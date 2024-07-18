3c1373b03aa1dfbbd62319642e63c1dec524a9de
Bjorn Johnson <bjornkjohnson@gmail.com>
2024-07-18 15:47:32 -0700

    Add test framework setup

---
+++ CoffeeApp/tests/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from CoffeeApp.config import settings
+from CoffeeApp.db import Base, get_db
+from main import app
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.TEST_DB_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+
+    # Create the database
+
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+
+    # Dependency override
+
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    yield TestClient(app)
