0c880215b444bfd385e21b18b0475d5002a3a8ae
Charles Knepshield <aknepshield@gmail.com>
2024-07-24 10:54:57 -0500

    Added create coffee shop test

--- CoffeeApp/tests/test_coffee_shops.py
+++ CoffeeApp/tests/test_coffee_shops.py
@@ -2,3 +2,10 @@ def test_get_coffee_shop(client):
     response = client.get("/coffee_shops")
     assert response.status_code == 200
     assert response.json() == []
+
+
+def test_create_coffee_shop(client):
+    payload = {"name": "example name"}
+    response = client.post("/coffee_shops/", json=payload)
+    assert response.status_code == 200
+    assert response.json()["name"] == "example name"
